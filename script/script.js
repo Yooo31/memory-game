9283f73200e7199d2ea403ffcadad7942cec88d9
Yooo31 <mrts.yoan@gmail.com>
2024-02-07 18:13:43 +0100

    On click, the picture is change for the equivalent

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
-function createCard(CardUrl) {
+function createCard(CardUrl, index) {
+    console.log(index);
     var card = document.createElement('div');
     card.classList.add('card');
-    card.dataset.value = CardUrl;
+    card.dataset.value = index.toString();
     var cardContent = document.createElement('img');
     cardContent.classList.add('card-content');
     cardContent.src = CardUrl;
@@ -31,7 +32,25 @@ var cards = [
 var allCards = duplicateArray(cards);
 allCards = shuffleArray(allCards);
 var gameBoard = document.getElementById('game-board');
-allCards.forEach(function (card) {
-    var cardElement = createCard(card);
+allCards.forEach(function (card, index) {
+    var cardElement = createCard('src/image.png', index);
     gameBoard.appendChild(cardElement);
 });
+function changeCardView(elementClicked) {
+    var card = document.querySelector("[data-value=\"".concat(elementClicked, "\"]"));
+    var cardContent = card === null || card === void 0 ? void 0 : card.querySelector('.card-content');
+    if (cardContent) {
+        cardContent.src = allCards[parseInt(elementClicked)];
+    }
+}
+var cardElements = document.querySelectorAll('.card');
+if (cardElements) {
+    cardElements.forEach(function (cardElement) {
+        cardElement.addEventListener('click', function () {
+            var dataValue = cardElement.getAttribute('data-value');
+            if (dataValue) {
+                changeCardView(dataValue);
+            }
+        });
+    });
+}
--- script/script.ts
+++ script/script.ts
@@ -1,7 +1,8 @@
-function createCard(CardUrl : string) : HTMLDivElement {
+function createCard(CardUrl : string, index : number) : HTMLDivElement {
+    console.log(index);
     const card = document.createElement('div');
     card.classList.add('card');
-    card.dataset.value = CardUrl;
+    card.dataset.value = index.toString();
 
     const cardContent = document.createElement('img');
     cardContent.classList.add('card-content');
@@ -41,7 +42,32 @@ let allCards : string[] = duplicateArray(cards);
 allCards = shuffleArray(allCards);
 const gameBoard = document.getElementById('game-board')!;
 
-allCards.forEach((card) => {
-  const cardElement = createCard(card);
+allCards.forEach((card, index) => {
+  const cardElement = createCard('src/image.png', index);
   gameBoard.appendChild(cardElement);
 });
+
+function changeCardView(elementClicked : string) {
+  const card = document.querySelector(`[data-value="${elementClicked}"]`);
+  const cardContent = card?.querySelector('.card-content') as HTMLImageElement | null;
+
+  if (cardContent) {
+    cardContent.src = allCards[parseInt(elementClicked)];
+  }
+}
+
+
+const cardElements: NodeListOf<HTMLElement> = document.querySelectorAll('.card');
+
+if (cardElements) {
+  cardElements.forEach((cardElement: HTMLElement) => {
+    cardElement.addEventListener('click', () => {
+      const dataValue: string | null = cardElement.getAttribute('data-value');
+
+      if (dataValue) {
+          changeCardView(dataValue);
+      }
+    });
+  });
+}
+
